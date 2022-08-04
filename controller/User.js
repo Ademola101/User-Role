f61d7d5bec1113d8525dac5ecfb503e0784fd2cf
Ademola <ademolaogunmokun@yahoo.com>
2022-08-04 20:04:59 +0100

    create controller

--- controller/User.js
+++ controller/User.js
@@ -3,4 +3,23 @@ const User = require('../models/User');
 
 const createUser = async (req, res) => {
   const { username, name, password } = req.body;
+
+  const saltround = 10;
+  const passwordHash = await bcrypt.hash(password, saltround);
+
+  const newUser = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: 'created',
+    data: newUser,
+  });
+};
+
+module.exports = {
+  createUser,
 };
